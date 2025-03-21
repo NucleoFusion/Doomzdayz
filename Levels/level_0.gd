1b879a0cb6da9796fe3f300cf1eff17c0501eefa
sumit-gupta-git <sumit.gupta.14471@gmail.com>
2025-03-21 11:32:05 +0530

    level manager

--- Levels/level_0.gd
+++ Levels/level_0.gd
@@ -1,15 +1,23 @@
 extends Node2D
 
 @export var EnemyNormal: PackedScene
+@export var EnemyList = []
 
 @onready var _player
+@onready var _levelTimer =  false
+@onready var _gameManager
 
 
 func _ready():
 	_player = get_tree().current_scene.get_node_or_null("GameScene/Player/CharacterBody2D")
-	
+	var _gameManager = get_tree().current_scene.get_node("GameScene/GameManager")
+
 	for i in range(5):
 		create_enemy()
+
+func _process(delta: float) :
+	if _levelTimer and EnemyList.size() ==0:
+		_gameManager.Level += 1 
 	
 func rand_coord():
 	var screen = get_viewport_rect().size
@@ -18,8 +26,8 @@ func rand_coord():
 	var x_range = randf_range(0, screen.x)
 	var y_range = randf_range(0,screen.y)
 	
-	if x_range > playerPos.x - float(100) and x_range < playerPos.y + float(100):
-		if  y_range > playerPos.y - float(100) and y_range < playerPos.y + float(100):
+	if x_range > playerPos.x - float(200) and x_range < playerPos.y + float(200):
+		if  y_range > playerPos.y - float(200) and y_range < playerPos.y + float(200):
 			return rand_coord()	
 	
 	return Vector2(x_range,y_range)
@@ -27,8 +35,14 @@ func rand_coord():
 func create_enemy():
 	var enemy = EnemyNormal.instantiate()
 	enemy.global_position = rand_coord()
+	EnemyList.append(enemy)
 	
 	get_tree().current_scene.add_child.call_deferred(enemy)
 
 func _on_spawn_time_timeout():
-	create_enemy()
+	if !_levelTimer:
+		create_enemy()
+
+
+func _on_level_timer_timeout() -> void:
+	_levelTimer = true
